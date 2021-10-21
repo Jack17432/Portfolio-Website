c9b6ad077220e971e1bcb34357b7478aaee6ae8b
Christopher Graham <Chrisgraham908@gmail.com>
2021-10-22 12:55:25 +1300

    Did the accordian andrew :)

---
+++ src/pages/projects/Apps.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../../components/organisms/layout/layout";
+
+export default function Home() {
+    return (
+        <Layout>
+            Apps
+        </Layout>
+    )
+}
