6d587735064640184d2f719febe4676f6c4885b5
Christopher Graham <Chrisgraham908@gmail.com>
2021-10-24 15:55:53 +1300

    Got the colision detection working

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -17,6 +17,38 @@ export default function Home() {
     const floor = -100;
     const roof = -500;
 
+    function getDist(obj1, obj2) {
+        let dist = 0;
+        let pos1 = allParticlePos[obj1];
+        let pos2 = allParticlePos[obj2];
+
+        let temp1 = [0, 0];
+        let temp2 = [0, 0];
+
+        temp1[0] = pos1[0] + 10;
+        temp1[1] = pos1[1] + 10;
+        temp2[0] = pos2[0] + 10;
+        temp2[1] = pos2[1] + 10;
+
+        let xDif = temp1[0] - temp2[0];
+        let yDif = temp1[1] - temp2[1];
+
+        dist  = Math.sqrt(xDif**2 + yDif**2);
+
+        let angle = 0;
+        if (-1 < (yDif / xDif) && 1 > (yDif / xDif)) {
+            angle = Math.asin(yDif / xDif);
+        } else {
+            angle = Math.asin(xDif / yDif);
+        }
+
+        return [dist, angle];
+    }
+
+    function collision(obj1, obj2, info) {
+        // TODO, to collision work
+    }
+
     function fire(obj, allParticle, allParticlePos, allParticleVel) {
         const particle = document.createElement('div');
         particle.className = ball;
@@ -41,27 +73,32 @@ export default function Home() {
             allParticlePos[i][0] = allParticlePos[i][0] + allParticleVel[i][0];
             allParticleVel[i][1] = allParticleVel[i][1] - gravity;
 
-            // Check if ball is outside of field
-            for(let i = 0; i < allParticle.length; i++){
 
-                if (allParticlePos[i][1] > floor || allParticlePos[i][1] < roof) {
-                    allParticleVel[i][1] = -allParticleVel[i][1] * friction;
-
-                    if (allParticlePos[i][1] > floor) {
-                        allParticlePos[i][1] = floor;
-                    } else {
-                        allParticlePos[i][1] = roof;
-                    }
+            // Check for hit
+            for(let x = i + 1; x < allParticle.length; x++){
+                const diffDist = getDist(i, x);
+                if (diffDist[0] < 20){
+                    console.log(diffDist);
                 }
+            }
 
-                if (leftWall > allParticlePos[i][0] || allParticlePos[i][0] > rightWall) {
-                    allParticleVel[i][0] = -allParticleVel[i][0] * friction;
+            if (allParticlePos[i][1] > floor || allParticlePos[i][1] < roof) {
+                allParticleVel[i][1] = -allParticleVel[i][1] * friction;
 
-                    if (leftWall > allParticlePos[i][0]) {
-                        allParticlePos[i][0] = leftWall;
-                    } else {
-                        allParticlePos[i][0] = rightWall
-                    }
+                if (allParticlePos[i][1] > floor) {
+                    allParticlePos[i][1] = floor;
+                } else {
+                    allParticlePos[i][1] = roof;
+                }
+            }
+
+            if (leftWall > allParticlePos[i][0] || allParticlePos[i][0] > rightWall) {
+                allParticleVel[i][0] = -allParticleVel[i][0] * friction;
+
+                if (leftWall > allParticlePos[i][0]) {
+                    allParticlePos[i][0] = leftWall;
+                } else {
+                    allParticlePos[i][0] = rightWall
                 }
             }
         }
